0f7bdc842063abb877b8d0dd9371de147dd13590
Faustin UHIRIWE <uhiriwe24@gmail.com>
2026-03-13 13:14:29 +0200

    Cleanup: Remove redundant and erroneous empty.py file

--- empty.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('tracking/', include('tracking.urls')),
-    path('payments/', include('payments.urls')),
-    path('audit/', include('audit.urls')),
-]
